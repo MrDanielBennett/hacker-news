ce0266a95f8cd2fa94816a94c1288bd87445b436
Daniel Bennett and Quinn Miller <quinn@>
2018-12-11 11:02:12 -0800

    add abitlity to upvote posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,37 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
+    @posts = Post.order('points DESC').all
     render :index
   end
+
   def show
     @post = Post.find(params[:id])
     render :show
   end
+
   def new
+    @post = Post.new
+    render :new
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    # binding.pry
+    render :show
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to post_path(@post)
+    else
+      render :new
+    end
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :link, :points)
   end
 end
